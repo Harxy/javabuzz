f8e88784a5b078a4c766a0f1d6ebe4243e5c778a
Paul Harker <harkerp@gmail.com>
2015-07-06 11:54:44 +0100

    added a failing test

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -4,13 +4,17 @@ describe('Javabuzz', function() {
 
   describe('knows when a number is', function() {
 
-    it('divisible by 3', function(){
+    it('isDivisibleByThree', function() {
       javabuzz = new Javabuzz();
-      expect(isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
-
-
   });
 
+  describe('knows when a number is NOT', function() {
 
+    it('divisible by three', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
 });
